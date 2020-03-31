e69c1f5b5a1320c6a7b7847c457f6105653e6157
Androz2091 <androz2091@gmail.com>
2020-03-31 10:07:23 +0200

    eval

--- index.js
+++ index.js
@@ -73,4 +73,29 @@ client.on("message", async (message) => {
     }
 });
 
+const clean = text => {
+    if (typeof(text) === "string")
+      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    else
+        return text;
+  }
+
+client.on("message", message => {
+    const args = message.content.split(" ").slice(1);
+   
+    if (message.content.startsWith("!eval")) {
+      try {
+        const code = args.join(" ");
+        let evaled = eval(code);
+   
+        if (typeof evaled !== "string")
+          evaled = require("util").inspect(evaled);
+   
+        message.channel.send(clean(evaled), {code:"xl"});
+      } catch (err) {
+        message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+      }
+    }
+  });
+
 client.login(config.token);
